24cf2f21446c6858ca3584bcd57ac048436f9608
Mike Gretton <mikegretton@gmail.com>
2020-05-05 21:33:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,3 +5,15 @@ playlist = {
 function updatePlaylist(obj, name, title) {
   return obj.Assign({}, obj, { name: title });
 }
+
+function removeFromPlaylist(obj, name) {
+    return { 
+      ...Object.keys(obj)
+          .filter(item => !isInArray(item, [name]))
+          .reduce((newObj, item) => { 
+              return {
+                ...newObj, [item]: obj[item]
+              }
+            }, {})
+    };
+}
